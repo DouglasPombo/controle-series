38e96ab55f760e403cb1e42e5f8b747e640b0c4e
douglas <douglaspombo2@gmail.com>
2020-09-06 13:46:30 -0300

    MVC - View Aula 3 (video 4)

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,19 +1,10 @@
-<!doctype html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-</head>
-<body>
-<div class="container">
-    <div class="jumbotron">
-        <h1>Adicionar Série</h1>
-    </div>
+@extends('layout')
 
+@section('cabecalho')
+    Adicionar Série
+@endsection
+
+@section('conteudo')
     <form method="post">
         <div class="form-group">
             <label for="nome">Nome</label>
@@ -21,6 +12,4 @@
         </div>
         <button class="btn btn-primary">Adicionar</button>
     </form>
-
-</body>
-</html>
+@endsection
--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -1,28 +1,15 @@
-<!doctype html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-</head>
-<body>
-    <div class="container">
-        <div class="jumbotron">
-            <h1>Séries</h1>
-        </div>
+@extends('layout')
 
+@section('cabecalho')
+    Séries
+@endsection
+
+@section('conteudo')
         <a href="/series/criar" class="btn btn-dark mb-2">Adicionar</a>
-        <ul class='list-group'>
-            <?php
-                foreach ($series as $serie){
-                    echo '<li class="list-group-item">'.$serie.'</li>';
-                }
-            ?>
-        </ul>
-    </div>
 
-</body>
-</html>
+        <ul class='list-group'>
+            @foreach ($series as $serie)
+                <li class="list-group-item"><?= $serie ?></li>
+            @endforeach
+        </ul>
+@endsection
