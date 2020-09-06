fb083cc798b4bb4ad7bd19adc2354d5fe73ceb2f
douglas <douglaspombo2@gmail.com>
2020-09-06 12:10:20 -0300

    MVC - View Aula 3 (video 2)

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -6,14 +6,23 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 </head>
 <body>
+    <div class="container">
+        <div class="jumbotron">
+            <h1>Séries</h1>
+        </div>
 
-    <?php
-        foreach ($series as $serie){
-            echo '<li>'.$serie.'</li>';
-        }
-    ?>
+        <a href="" class="btn btn-dark mb-2">Adicionar</a>
+        <ul class='list-group'>
+            <?php
+                foreach ($series as $serie){
+                    echo '<li class="list-group-item">'.$serie.'</li>';
+                }
+            ?>
+        </ul>
+    </div>
 
 </body>
 </html>
