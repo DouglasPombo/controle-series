dff4d8ee741bbd6273dedcbc28ce11fcdd91d0c7
douglas <douglaspombo2@gmail.com>
2020-09-07 22:13:09 -0300

    Relacionamentos no modelo Aula 1 (video 4)

---
+++ database/migrations/2020_09_06_172431_criar_tabela_series.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CriarTabelaSeries extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+       Schema::create('series',function(Blueprint $table){
+            $table->String('nome');
+            $table->increments('id');
+       });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('series');
+    }
+}
