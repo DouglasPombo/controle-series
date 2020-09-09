ee995bd3456413c2cb1e786683c6c5d8a6b1eb0c
Douglas pombo <douglaspomboot@gmail.com>
2020-09-09 16:34:15 -0300

    Novo controller e view Aula 02 (video 1)

--- app/Http/Controllers/seriesController.php
+++ app/Http/Controllers/seriesController.php
@@ -12,7 +12,6 @@ class seriesController extends Controller
     public function index(Request $request)
     {
         $series = Serie::query()->orderBy('nome')->get();
-
         $mensagem = $request->session()->get('mensagem');
 
 
@@ -27,8 +26,20 @@ class seriesController extends Controller
 
     public function store(SeriesFormRequest $request)
     {
+        $serie = Serie::create(['nome' => $request->nome]);
 
-        $serie = Serie::create($request->all());
+        dd($serie);
+        $qtdTemporadas = $request->qtd_temporadas;
+
+
+        for($i=1; $i <= $qtdTemporadas; $i++){
+            $temporada = $serie->temporadas()->create(['numero' => $i]);
+
+
+            for($j=1; $j <= $request->ep_por_temporada; $j++){
+                $temporada->episodios()->create(['numero' => $j]);
+            }
+        }
         $request->session()->flash('mensagem',"Série {$serie->id} criada com sucesso {$serie->nome}");
 
         return redirect()->route('listar_series');
--- app/Temporada.php
+++ app/Temporada.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Temporada extends Model
 {
     public $timestamps = false;
+    protected $fillable = ['numero'];
 
     public function episodios()
     {
