e54e71eba0f57c2f8277ce3183f0196617cb1a7a
Douglas pombo <douglaspomboot@gmail.com>
2020-09-10 17:21:27 -0300

    Usando serviços para exclusão Aula 04 (video 1)

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -16,8 +16,21 @@
             @foreach ($series as $serie)
 
                     <li class="list-group-item d-flex justify-content-between align-items-center">
-                        {{$serie->nome}}
+                        <span id="nome-serie-{{ $serie->id }}">{{ $serie->nome }}</span>
+
+                        <div class="input-group w-50" hidden id="input-nome-serie-{{ $serie->id }}">
+                            <input type="text" class="form-control" value="{{ $serie->nome }}">
+                            <div class="input-group-append">
+                                <button class="btn btn-primary" onclick="editarSerie({{ $serie->id }})">
+                                    <i class="fas fa-check"></i>
+                                </button>
+                                @csrf
+                            </div>
+                        </div>
                         <span class="d-flex">
+                            <button class="btn btn-info btn-sm mr-1" onclick="toggleInput({{$serie->id}})">
+                                <i class="fas fa-edit"></i>
+                            </button>
                         <a href="/series/{{$serie->id}}/temporadas" class="btn btn-info btn-sm  mr-1">
                             <i class="fas fa-external-link-alt"></i>
                         </a>
@@ -31,4 +44,22 @@
                     </li>
             @endforeach
         </ul>
+
+<script>
+    function toggleInput(serieId){
+
+        var nomeSerie = document.getElementById('input-nome-serie-' + serieId + '');
+        var inputSerie = document.getElementById('nome-serie-' + serieId + '');
+
+        if(nomeSerie.hasAttribute('hidden')){
+            nomeSerie.removeAttribute('hidden');
+            inputSerie.hidden = true;
+            return;
+        }
+
+        inputSerie.removeAttribute('hidden');
+        nomeSerie.hidden = true;
+
+    }
+</script>
 @endsection
