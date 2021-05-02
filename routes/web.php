84d033c4de9a7a4e742d76b76871c224033f1a40
Ghujio <douglaspombo2@gmail.com>
2021-05-02 20:54:50 -0300

    criando autenticação de usuários

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,17 @@ Route::post('/series/criar', 'seriesController@store');
 Route::post('/series/{id}/editaNome', 'seriesController@editaNome');
 Route::delete('/series/{id}', 'seriesController@destroy');
 Route::get('/series/editar', 'seriesController@edit');
+Route::get('/entrar', 'EntrarController@index');
+Route::post('/entrar', 'EntrarController@entrar');
+Route::get('/registrar', 'RegistroController@create');
+Route::post('/registrar', 'RegistroController@store');
 
 
 Route::get('series/{serieId}/temporadas', 'TemporadasController@index');
 
 Route::get('/temporadas/{temporada}/episodios', 'EpisodiosController@index');
 Route::post('/temporadas/{temporada}/episodios/assistir', 'EpisodiosController@assistir');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
